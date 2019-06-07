0d28bcb37343c6da63109e48d7f97b9f4238a71d
August Ye <augustye@gmail.com>
2019-06-07 21:48:45 +0800

    add mnist support

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from flask import Flask,request,jsonify,render_template,make_response
+from PIL import Image, ImageEnhance, ImageOps
+from scipy import misc
+from array import *
+import numpy as np
 import urllib.request
 import base64
+import cv2
+import io
 
 app = Flask(__name__)
 try:
-    import pynq
-    pynq_overlay = pynq.Overlay("base.bit")
+  import pynq
+  import bnn
+  pynq_overlay = pynq.Overlay("base.bit")
+  mnist_classifier = bnn.LfcClassifier(bnn.NETWORK_LFCW1A1,"mnist",bnn.RUNTIME_HW)
 except ImportError:
-    pynq = None
+  pynq = None
 
 @app.route('/extension/<name>')
 def extension_led(name):
@@ -30,12 +38,36 @@ def led():
 
 @app.route('/mnist', methods = ['POST'])
 def mnist():
-  img_data = request.get_json().get('data')
   if pynq is None:
-      return jsonify({"error":"Error: pynq is not found"})
-  with open("imageToSave.png", "wb") as fh:
-    status = fh.write(base64.b64decode(img_data))
-  return jsonify(status)
+    return jsonify({"error":"Error: pynq is not found"})
+  img_data = request.get_json().get('data')
+  img_data = base64.b64decode(img_data)
+  img = Image.open(io.BytesIO(img_data))
+  img = img.convert("L")   
+  img = ImageEnhance.Contrast(img).enhance(3)  
+  img = ImageEnhance.Brightness(img).enhance(4.0)  
+  box = ImageOps.invert(img).getbbox()  
+  img = img.crop(box) 
+  width, height = img.size  
+  ratio = min((28./height), (28./width))  
+  smallimg = Image.new('RGB', (28,28), (255,255,255))  
+  if(height>width):  
+      img = img.resize((int(width*ratio),28))  
+      smallimg.paste(img, (int((28-img.size[0])/2),int((28-img.size[1])/2)))  
+  else:  
+      img = img.resize((28, int(height*ratio)))  
+      smallimg.paste(img, (int((28-img.size[0])/2),int((28-img.size[1])/2)))  
+  smallimg = ImageOps.invert(smallimg).convert("L")  
+  #smallimg.save("smallimg.png")
+  data_image = array('B', [0, 0, 8, 3, 0, 0, 0, 1, 0, 0, 0, 28, 0, 0, 0, 28])
+  pixel = smallimg.load()  
+  for x in range(0,28):  
+    for y in range(0,28):  
+        data_image.append(255 if pixel[y,x] == 255 else 1)  
+  with open('/tmp/mnist_processed', 'wb') as file:
+    data_image.tofile(file)  
+  result = mnist_classifier.classify_mnist('/tmp/mnist_processed')
+  return jsonify({"class":result})
 
 @app.after_request
 def after_request(response):
